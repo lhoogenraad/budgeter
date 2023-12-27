4980bd16821a35a3782ceccafddd5bc9b4c71742
Leon <le.o.n@outlook.com>
2023-12-27 16:06:17 +1300

    made db

---
+++ server/src/general/db.js
@@ -0,0 +1,15 @@
+const { Client } = require("pg");
+require('dotenv').config();
+
+const dbConfig = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: true,
+}
+
+const client = new Client(dbConfig);
+
+export default client;
--- server/src/services/users.service.js
+++ server/src/services/users.service.js
@@ -1,1 +1,6 @@
 console.log("user service")
+const db = require("../general/db");
+
+exports.login = (email, password) => {
+	console.log(email, password)
+}
